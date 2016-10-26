0768188e9e1865ebbe42199a275bca0661b6b97f
catmando <mitch@catprint.com>
2016-10-26 14:28:52 -0400

    need to include opal first

--- lib/hyper-trace.rb
+++ lib/hyper-trace.rb
@@ -2,5 +2,6 @@ if RUBY_ENGINE=='opal'
   require 'hyper_trace/hyper_trace.rb'
   require 'hyper_trace/react_trace.rb'
 else
+  require 'opal'
   Opal.append_path File.expand_path('../', __FILE__).untaint
 end
